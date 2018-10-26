0722ff4a586cbd6449fb010f03f41c7903deeb44
ybxxszl <1062837400@qq.com>
2018-10-26 21:38:08 +0800

    mq

---
+++ src/main/java/com/wjy/mq/Demo.java
@@ -0,0 +1,19 @@
+package com.wjy.mq;
+
+import org.junit.Test;
+
+/**
+ * @Date 2018/10/26
+ * @Author ybxxszl
+ * @Desc
+ **/
+public class Demo {
+
+    @Test
+    public void test() {
+
+        System.out.println("nice");
+
+    }
+
+}
